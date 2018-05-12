f5aa84b89aac54ef1cdefab34f64c507d38ff1c1
Roman Gordienko <r.u.gordienko@gmail.com>
2018-05-12 19:27:50 +0300

    redux-observable basic implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { WorkareaModule } from './workarea.module';
 import { NavigationModule } from './navigation.module'
 import { NgReduxModule, NgRedux, DevToolsExtension } from '@angular-redux/store';
+import { createEpicMiddleware } from 'redux-observable';
 
 import { AppComponent } from './components/app.component';
 
 import rootReducer from './redux/reducers';
+import rootEpic from './redux/epics';
 import { IAppState, initialState } from './redux';
 
 @NgModule({
@@ -27,10 +29,15 @@ export class AppModule {
     ngRedux: NgRedux<IAppState>,
     devTools: DevToolsExtension
   ) {
-    let enhancers = [];
+    const middlewares = [];
+    const enhancers = [];
+
+    const epicMiddleware = createEpicMiddleware(rootEpic);
+    middlewares.push(epicMiddleware);
+
     if (devTools.isEnabled()) {
       enhancers.push(devTools.enhancer());
     };
-    ngRedux.configureStore(rootReducer, initialState, [], enhancers);
+    ngRedux.configureStore(rootReducer, initialState, middlewares, enhancers);
   }
 }
